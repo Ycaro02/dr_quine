3a63b1d7067a4afd8b468ef6ee9aa7990df808b9
Ycaro02 <ycaro@Kali.home>
2024-02-18 17:14:17 -0600

    Clean Grace ok

--- src/Grace/C/Grace.c
+++ src/Grace/C/Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define NO_MAIN main
-#define a "#include <stdio.h>%c#define NO_MAIN main%c#define a %c%s%c%c%c#define TT int NO_MAIN(){printf(a,10,10,34,a,34,10,10,10);return(0);}%cTT"
+#define a "#include <stdio.h>%c#define NO_MAIN main%c#define a %c%s%c%c%c#define START_MACRO int NO_MAIN(){/*Ez*/printf(a,10,10,34,a,34,10,10,10);return(0);}%cSTART_MACRO"
 
-#define TT int NO_MAIN(){printf(a,10,10,34,a,34,10,10,10);return(0);}
-TT
+#define START_MACRO int NO_MAIN(){/*Ez*/printf(a,10,10,34,a,34,10,10,10);return(0);}
+START_MACRO
