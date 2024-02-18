a71590b7fe7bae8350c338eb3fbf6df156117484
Ycaro02 <ycaro@Kali.home>
2024-02-18 17:10:47 -0600

    Grace_ok

--- src/Grace/C/Grace.c
+++ src/Grace/C/Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define NO_MAIN main
-#define a "#include <stdio.h>%c#define NO_MAIN main%c#define a %c%s%c%c%c#define TT int NO_MAIN(){printf(a,10,10,34,a,10,34,10,10);return(0);}%cTT"
+#define a "#include <stdio.h>%c#define NO_MAIN main%c#define a %c%s%c%c%c#define TT int NO_MAIN(){printf(a,10,10,34,a,34,10,10,10);return(0);}%cTT"
 
-#define TT int NO_MAIN(){printf(a,10,10,34,a,10,34,10,10);return(0);}
+#define TT int NO_MAIN(){printf(a,10,10,34,a,34,10,10,10);return(0);}
 TT
