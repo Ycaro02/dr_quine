02f13e49d58fea0664c45419be2431191935009c
Ycaro02 <ycaro02@hotmailfr>
2024-02-19 18:11:41 +0100

    word on sully

--- src/Sully/C/Sully.c
+++ src/Sully/C/Sully.c
@@ -6,15 +6,16 @@
 #include <unistd.h>
 #include <string.h>
 int main(){
-char src[30];
-char des[30];
+char bin[30];
+char name[30];
 int x = 5;
-char*a="#include <stdio.h>%c#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <string.h>%cint main(){%cchar src[30];%cchar des[30];%cint x = 5;%cchar*a=%c%s%c;%cif (access(%cSully_5.c%c, R_OK) != -1){%c--x;}%cstrcpy(des, %cSully_%c);%cdes[6] = x + 48;%cdes[7] = 0;%cstrcat(des, %c.c%c);%cint fd = open(des, O_CREAT | O_RDWR, 00777);%cdprintf(fd,a,10,10,10,10,10,10,10,10,10,10,10,34,a,34,10,34,34,10,10,34,34,10,10,10,34,34,10,10);return(0);}";
+char*a="#include <stdio.h>%c#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <string.h>%cint main(){%cchar bin[30];%cchar name[30];%cint x = %d;%cchar*a=%c%s%c;%cif (access(%cSully_5.c%c, R_OK) != -1){%c--x;}%cstrcpy(name, %cSully_%c);%cname[6] = x + 48;%cname[7] = 0;%cstrcpy(bin, name);%cstrcat(name, %c.c%c);%cint fd = open(name, O_CREAT | O_RDWR, 00777);%cdprintf(fd,a,10,10,10,10,10,10,10,10,10,10,x,10,34,a,34,10,34,34,10,10,34,34,10,10,10,10,34,34,10,10);return(0);}";
 if (access("Sully_5.c", R_OK) != -1){
 --x;}
-strcpy(des, "Sully_");
-des[6] = x + 48;
-des[7] = 0;
-strcat(des, ".c");
-int fd = open(des, O_CREAT | O_RDWR, 00777);
-dprintf(fd,a,10,10,10,10,10,10,10,10,10,10,10,34,a,34,10,34,34,10,10,34,34,10,10,10,34,34,10,10);return(0);}
+strcpy(name, "Sully_");
+name[6] = x + 48;
+name[7] = 0;
+strcpy(bin, name);
+strcat(name, ".c");
+int fd = open(name, O_CREAT | O_RDWR, 00777);
+dprintf(fd,a,10,10,10,10,10,10,10,10,10,10,x,10,34,a,34,10,34,34,10,10,34,34,10,10,10,10,34,34,10,10);return(0);}
