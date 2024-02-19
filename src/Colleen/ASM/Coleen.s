82e50e87dbcf429b24f0dfca7b6aea5c78d9d818
Ycaro02 <ycaro02@hotmailfr>
2024-02-19 21:37:17 +0100

    setup asm compiling realy a pain 3 link in dox

--- src/Colleen/ASM/Coleen.s
+++ src/Colleen/ASM/Coleen.s
@@ -1,8 +1,13 @@
 global main
 extern printf
 
-section .text
+default rel          ; RIP-relative addressing instead of 32-bit absolute by default; makes the [rel ...] optional
 
+section .rodata
+format:
+	db "global main%1$cextern printf%1$c%1$c", 0
+
+section .text
 colleen:
 	push rbx
 	lea rdi, format
@@ -10,7 +15,7 @@ colleen:
 	mov rdx, 9
 	mov rcx, 34
 	lea r8, format  ; lea load effective address : https://www.developpez.net/forums/d304413/autres-langages/assembleur/8086-signification-l-instruction-lea/
-	call printf
+	call [rel printf wrt ..got]
 	pop rbx
 	ret
 
@@ -24,6 +29,3 @@ main:
 	mov rax, 0	; return 0
 	ret
 
-section .data
-format:
-	db ";lobal main%1$cextern printf%1$c%1$csection .text%1$ccolleen:%1$c%2$cpush rbx%1$c%2$cmov rdi, format%1$c%2$cmov rsi, 10%1$c%2$cmov rdx, 9%1$c%2$cmov rcx, 34%1$c%2$cmov r8, format%1$c%2$ccall printf%1$c%2$cpop rbx%1$c%2$cret%1$c%1$cmain:%1$c%2$cpush rbx%1$c%2$ccall colleen%1$c%2$cpop rbx%1$c%1$c%2$c;%1$c%2$c;%2$cCOMMENT2$cmov rax, 0%2$c; return 0%1$c%2$cret%1$c%1$csection .data%1$cformat:%1$c%2$cdb %3$c%4$s%3$c, 0%1$c", 0
