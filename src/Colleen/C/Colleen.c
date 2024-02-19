dbcf964357c8c56cdbea54fe5bceeb53364aeba7
Ycaro02 <ycaro02@hotmailfr>
2024-02-19 12:15:36 +0100

    Colleen clean with required comment

--- src/Colleen/C/Colleen.c
+++ src/Colleen/C/Colleen.c
@@ -4,6 +4,9 @@
 */
 int get_new_line(){return(10);}
 
-#define a "#include <stdio.h>%c/*%c%cSecond here%c*/%cint get_new_line(){return(10);}%c%c#define a %c%s%c%cint main(){%c/*First here*/printf(a,10,get_new_line(),9,10,10,10,10,34,a,34,10,10);return(0);}"
+#define a "#include <stdio.h>%c/*%c%cSecond here%c*/%cint get_new_line(){return(10);}%c%c#define a %c%s%c%cint main(){%c/*%c%cFirst here%c*/%cprintf(a,10,get_new_line(),9,10,10,10,10,34,a,34,10,10,10,9,10,10);return(0);}"
 int main(){
-/*First here*/printf(a,10,get_new_line(),9,10,10,10,10,34,a,34,10,10);return(0);}
+/*
+	First here
+*/
+printf(a,10,get_new_line(),9,10,10,10,10,34,a,34,10,10,10,9,10,10);return(0);}
