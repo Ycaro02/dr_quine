caa8b37f82ddc24c4c3fdfcd07a3daa5b284c748
Ycaro02 <ycaro02@hotmailfr>
2024-02-19 10:55:07 +0100

    Colleen upgrade comment need to clean first

--- src/Colleen/C/Colleen.c
+++ src/Colleen/C/Colleen.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+/*
+	Second here
+*/
+int get_new_line(){return(10);}
 
-int /*Second here*/get_new_line(){return(10);}
-
-#define a "#include <stdio.h>%c%cint /*Second here*/get_new_line(){return(10);}%c%c#define a %c%s%c%cint main(){%c/*First here*/printf(a,10,get_new_line(),10,10,34,a,34,10,10);return(0);}"
+#define a "#include <stdio.h>%c/*%c%cSecond here%c*/%cint get_new_line(){return(10);}%c%c#define a %c%s%c%cint main(){%c/*First here*/printf(a,10,get_new_line(),9,10,10,10,10,34,a,34,10,10);return(0);}"
 int main(){
-/*First here*/printf(a,10,get_new_line(),10,10,34,a,34,10,10);return(0);}
+/*First here*/printf(a,10,get_new_line(),9,10,10,10,10,34,a,34,10,10);return(0);}
