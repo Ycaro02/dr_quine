b4a926ba1c731c8330cb9f9b2713a0d5efbbcfa2
Ycaro02 <ycaro02@hotmailfr>
2024-02-19 17:47:17 +0100

    save SUlly

--- rsc/first.c
+++ rsc/first.c
@@ -24,4 +24,48 @@ int main(){int fd = open("Sully_X.c", O_CREAT | O_RDWR, 00777);dprintf(fd,a,10,1
 #include <sys/stat.h>
 #include <fcntl.h>
 
-// printf(a,10,10,10,10,10,34,a,34,10);return(0);}
+
+
+
+/* Sully printf:
+
+dprintf(fd,a,10,10,10,10,10,10,10,10,10,10,10,34,a,34,10,34,34,10,10,34,34,10)
+
+a: start display
+
+10:stdio:\n
+10:sys type:\n
+10:sys stat:\n
+10:sys fcntl:\n
+10:stdlib.h \n
+10:unistd \n
+10:strings \n
+
+10:main :\n
+10:char *src :\n
+10:char *dest :\n
+10:int x = :\n
+
+34-start char *a
+a: display a def 
+34: end def
+10: def \n
+
+
+
+34 if access sully 5 quote start
+34 if access sully 5 quote end
+10 if access \n
+10 --x} \n
+34 strcpy quote start
+34 strcpy quote end
+10 strcpy \n
+10 des[6] = x \n
+10 des[7] = \n
+
+34 strcat .c quotes start
+34 strcat .c quotes end
+10 \n
+
+10: int fd : \n
+*/
--- src/Sully/C/Sully_X.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-int main(){
-char*a="#include <stdio.h>%c#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>%cint main(){%cchar*a=%c%s%c;%cint fd = open(%cSully_X.c%c, O_CREAT | O_RDWR, 00777);%cdprintf(fd,a,10,10,10,10,10,34,a,34,10,34,34,10);return(0);}";
-int fd = open("Sully_X.c", O_CREAT | O_RDWR, 00777);
-dprintf(fd,a,10,10,10,10,10,34,a,34,10,34,34,10);return(0);}
