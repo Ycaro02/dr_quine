1d78e78722adead0175dc415c03a28e5c970b2cb
Ycaro02 <ycaro02@hotmailfr>
2024-02-19 10:38:59 +0100

    Sully comment

--- src/Sully/C/Sully.c
+++ src/Sully/C/Sully.c
@@ -1,7 +1,26 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdio.h>
+#define NO_MAIN main
+#define a "#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>%c#include <stdio.h>%c#define NO_MAIN main%c#define a %c%s%c%c%c#define START_MACRO int NO_MAIN(){int fd = open(%cGrace_kid.c%c, O_CREAT | O_RDWR, 00777);dprintf(fd, a,10,10,10,10,10,34,a,34,10,10,34,34,10);return(0);}%cSTART_MACRO"
 
-int /*Second here*/get_new_line(){return(10);}
+#define START_MACRO int NO_MAIN(){int fd = open("Grace_kid.c", O_CREAT | O_RDWR, 00777);dprintf(fd, a,10,10,10,10,10,34,a,34,10,10,34,34,10);return(0);}
+START_MACRO
 
-#define a "#include <stdio.h>%c%cint /*Second here*/get_new_line(){return(10);}%c%c#define a %c%s%c%cint main(){%c/*First here*/printf(a,10,get_new_line(),10,10,34,a,34,10,10);return(0);}"
-int main(){
-/*First here*/printf(a,10,get_new_line(),10,10,34,a,34,10,10);return(0);}
+
+
+/*  - Named Sully
+    - He write his output un shully_X.c
+        -The X will be an interger given in the source.
+        - Once the file is created, the program compiles this file and then 
+        runs the new program (which will have the name of its source file)
+    - Stopping the program depends on the file name : 
+    the resulting program will be executed only if the integer X 
+    is greater than 0. 
+    - An integer is therefore present in the source of your program and will 
+    have to evolve by decrementing every time you create a source file
+    from the execution of the program.
+    -You have no constraints on the source code, apart from the integer 
+    that will be set to 5 at first.
+*/
