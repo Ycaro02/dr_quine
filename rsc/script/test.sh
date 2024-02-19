a93427a475f2c1c21dd28b2244f0d5f235647c19
Ycaro02 <ycaro02@hotmailfr>
2024-02-19 22:42:29 +0100

    clean directory struct and add colleen.s in tester

--- rsc/script/test.sh
+++ rsc/script/test.sh
@@ -8,29 +8,50 @@ BLUE="\e[34m"
 CYAN="\e[36m"
 RESET="\e[0m"
 
-BIN_NAME=test_diff
-
-TMP_OUT=.tmp_output
-
-GRACE_C_OUTPUT=src/C/Grace_kid.c
-
-GRACE_C=src/C/Grace.c
 
 COLLEEN_C=src/C/Colleen.c
+COLLEEN_ASM=src/ASM/Colleen.s
+COLEEN_OUT=COLEEN_output
+
+GRACE_C=src/C/Grace.c
+GRACE_C_OUTPUT=Grace_kid.c
 
 SULLY_C=src/C/Sully.c
-
 SULLY_CLEAR=src/C/clear_sully.sh
 
-clang -Wall -Wextra -Werror -o Colleen ${COLLEEN_C}; ./Colleen > ${TMP_OUT} ;
-diff ${TMP_OUT} ${COLLEEN_C} && echo -e "${YELLOW}${COLLEEN_C} ${RESET}${GREEN}OK${RESET}"
+CALL_ASM_DIR="make -s -C src/ASM"
+
+clang -Wall -Wextra -Werror -o Colleen ${COLLEEN_C}; ./Colleen > ${COLEEN_OUT};
+diff ${COLEEN_OUT} ${COLLEEN_C} 
+if [ $? -eq 0 ] ; then
+echo -e "${YELLOW}${COLLEEN_C}\t\t${RESET}${GREEN}OK${RESET}"
+else
+echo -e "${YELLOW}${COLLEEN_C}\t\t${RESET}${RED}KO${RESET}"
+fi
 
 clang -Wall -Wextra -Werror -o Grace ${GRACE_C}; ./Grace ;
-diff ${GRACE_C} Grace_kid.c  && echo -e "${YELLOW}${GRACE_C}${RES_COLOR}\t${GREEN}OK${RES_COLOR}"
+diff ${GRACE_C} ${GRACE_C_OUTPUT}
+if [ $? -eq 0 ] ; then
+echo -e "${YELLOW}${GRACE_C}\t\t${RESET}${GREEN}OK${RESET}"
+else
+echo -e "${YELLOW}${GRACE_C}\t\t${RESET}${RED}KO${RESET}"
+fi
 
 clang -Wall -Wextra -Werror -o Sully ${SULLY_C}; ./Sully ; 
-diff ${SULLY_C} Sully_5.c  && echo -e "${YELLOW}${SULLY_C}5${RES_COLOR}\t${GREEN}OK${RES_COLOR}"
+diff ${SULLY_C} Sully_5.c
+if [ $? -eq 0 ] ; then
+echo -e "${YELLOW}${SULLY_C}\t\t${RESET}${GREEN}OK${RESET}"
+else
+echo -e "${YELLOW}${SULLY_C}\t\t${RESET}${RED}KO${RESET}"
+fi
+
+${CALL_ASM_DIR} && ./src/ASM/Colleen > ${COLEEN_OUT};
+diff ${COLEEN_OUT} ${COLLEEN_ASM}
+if [ $? -eq 0 ] ; then
+echo -e "${YELLOW}${COLLEEN_ASM}\t${RESET}${GREEN}OK${RESET}"
+else
+echo -e "${YELLOW}${COLLEEN_ASM}\t${RESET}${RED}KO${RESET}"
+fi
 
 ./${SULLY_CLEAR}
-
-rm ${TMP_OUT} Colleen Sully Grace Grace_kid.c;
+rm ${COLEEN_OUT} Colleen Sully Grace Grace_kid.c;
