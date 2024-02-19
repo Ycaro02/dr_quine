46a385985b45a9d7f2fecc5cd4fb011352c0eb7a
Ycaro02 <ycaro02@hotmailfr>
2024-02-19 16:49:42 +0100

    add O_TRUNC for Grace

--- src/Grace/C/Grace.c
+++ src/Grace/C/Grace.c
@@ -6,7 +6,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #define NO_MAIN main
-#define a "#include <sys/types.h>%c/*%c%cHey%c*/%c#include <sys/stat.h>%c#include <fcntl.h>%c#include <stdio.h>%c#define NO_MAIN main%c#define a %c%s%c%c%c#define START_MACRO(x) int NO_MAIN(){int fd = open(%cGrace_kid.c%c, x, 00777);dprintf(fd, a,10,10,9,10,10,10,10,10,10,34,a,34,10,10,34,34,10);return(0);}%cSTART_MACRO(O_CREAT | O_RDWR)"
+#define a "#include <sys/types.h>%c/*%c%cHey%c*/%c#include <sys/stat.h>%c#include <fcntl.h>%c#include <stdio.h>%c#define NO_MAIN main%c#define a %c%s%c%c%c#define START_MACRO(x) int NO_MAIN(){int fd = open(%cGrace_kid.c%c, x, 00777);dprintf(fd, a,10,10,9,10,10,10,10,10,10,34,a,34,10,10,34,34,10);return(0);}%cSTART_MACRO(O_CREAT | O_WRONLY | O_TRUNC)"
 
 #define START_MACRO(x) int NO_MAIN(){int fd = open("Grace_kid.c", x, 00777);dprintf(fd, a,10,10,9,10,10,10,10,10,10,34,a,34,10,10,34,34,10);return(0);}
-START_MACRO(O_CREAT | O_RDWR)
+START_MACRO(O_CREAT | O_WRONLY | O_TRUNC)
