8706a893c61b4c781e92d93dcb66da9c34196644
Ycaro02 <ycaro02@hotmailfr>
2024-03-01 09:29:17 +0100

    sully bash create 0.sh now

--- src/BASH/Sully.sh
+++ src/BASH/Sully.sh
@@ -16,13 +16,13 @@ x=${SULLY}
 tmp=$(($x-1))
 export SULLY=$tmp
 fi
-if [ $x -le 0 ];
+if [ $x -le -1 ];
 then
 return 0;
 fi
 touch Sully_$x.sh
 chmod +x Sully_$x.sh
-s='#!/bin/bash\12CHECK_NAME=Sully_5.sh\12x=5\12# Comment here\12Sully(){\12if [ ! -f ${CHECK_NAME} ];\12then\12touch ${CHECK_NAME}\12export SULLY=4\12else\12if [ ! $SULLY ];\12then\12export SULLY=0\12fi\12x=${SULLY}\12tmp=$(($x-1))\12export SULLY=$tmp\12fi\12if [ $x -le 0 ];\12then\12return 0;\12fi\12touch Sully_$x.sh\12chmod +x Sully_$x.sh\12s=\47%s\47\12printf "$s" "$s" > Sully_$x.sh;\12./Sully_$x.sh\12}\12Sully\12'
+s='#!/bin/bash\12CHECK_NAME=Sully_5.sh\12x=5\12# Comment here\12Sully(){\12if [ ! -f ${CHECK_NAME} ];\12then\12touch ${CHECK_NAME}\12export SULLY=4\12else\12if [ ! $SULLY ];\12then\12export SULLY=0\12fi\12x=${SULLY}\12tmp=$(($x-1))\12export SULLY=$tmp\12fi\12if [ $x -le -1 ];\12then\12return 0;\12fi\12touch Sully_$x.sh\12chmod +x Sully_$x.sh\12s=\47%s\47\12printf "$s" "$s" > Sully_$x.sh;\12./Sully_$x.sh\12}\12Sully\12'
 printf "$s" "$s" > Sully_$x.sh;
 ./Sully_$x.sh
 }
