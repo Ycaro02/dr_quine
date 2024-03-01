f16b03dd7d544a3395e37ec05b4dd4702fdfc714
Ycaro02 <ycaro02@hotmailfr>
2024-03-01 08:42:18 +0100

    fix sully.c -1 cause leak

--- src/C/Sully.c
+++ src/C/Sully.c
@@ -9,7 +9,8 @@ int main(){
 char bin[30];
 char name[30];
 int x = 5;
-char*a="#include <stdio.h>%c#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <string.h>%cint main(){%cchar bin[30];%cchar name[30];%cint x = %d;%cchar*a=%c%s%c;%cif (access(%cSully_5.c%c, R_OK) != -1){%c--x;}%cstrcpy(name, %cSully_%c);%cname[6] = x + 48;%cname[7] = 0;%cstrcpy(bin, name);%cstrcat(name, %c.c%c);%cint fd = open(name, O_CREAT | O_RDWR, 00777);%cdprintf(fd,a,10,10,10,10,10,10,10,10,10,10,x,10,34,a,34,10,34,34,10,10,34,34,10,10,10,10,34,34,10,10,10,10,10,34,37,37,37,34,10,10,10);%cclose(fd);%cchar cmd[2000];%csprintf(cmd, %cgcc -o %cs %cs && ./%cs%c, bin,name,bin);%cif (x > 0){%csystem(cmd);}%creturn(0);}";
+char*a="#include <stdio.h>%c#include <sys/types.h>%c#include <sys/stat.h>%c#include <fcntl.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <string.h>%cint main(){%cchar bin[30];%cchar name[30];%cint x = %d;%cchar*a=%c%s%c;%cif (x < 0) {return (0);}%cif (access(%cSully_5.c%c, R_OK) != -1){%c--x;}%cstrcpy(name, %cSully_%c);%cname[6] = x + 48;%cname[7] = 0;%cstrcpy(bin, name);%cstrcat(name, %c.c%c);%cint fd = open(name, O_CREAT | O_RDWR, 00777);%cdprintf(fd,a,10,10,10,10,10,10,10,10,10,10,x,10,34,a,34,10,10,34,34,10,10,34,34,10,10,10,10,34,34,10,10,10,10,10,34,37,37,37,34,10,10,10);%cclose(fd);%cchar cmd[2000];%csprintf(cmd, %cgcc -o %cs %cs && ./%cs%c, bin,name,bin);%cif (x > 0){%csystem(cmd);}%creturn(0);}";
+if (x < 0) {return (0);}
 if (access("Sully_5.c", R_OK) != -1){
 --x;}
 strcpy(name, "Sully_");
@@ -18,7 +19,7 @@ name[7] = 0;
 strcpy(bin, name);
 strcat(name, ".c");
 int fd = open(name, O_CREAT | O_RDWR, 00777);
-dprintf(fd,a,10,10,10,10,10,10,10,10,10,10,x,10,34,a,34,10,34,34,10,10,34,34,10,10,10,10,34,34,10,10,10,10,10,34,37,37,37,34,10,10,10);
+dprintf(fd,a,10,10,10,10,10,10,10,10,10,10,x,10,34,a,34,10,10,34,34,10,10,34,34,10,10,10,10,34,34,10,10,10,10,10,34,37,37,37,34,10,10,10);
 close(fd);
 char cmd[2000];
 sprintf(cmd, "gcc -o %s %s && ./%s", bin,name,bin);
